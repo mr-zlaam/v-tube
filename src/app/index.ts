084fa5666812ba9ca0066cb202ba519ac42f101a
Zlaam <mrzalaam@gmail.com>
2024-05-31 18:52:16 +0500

    update src/app/index.ts and src/config/index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,23 @@
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import express from "express";
-
+import cors from "cors";
+import { CORS_ORIGIN } from "../config";
 export const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,7 @@ const _config = {
   ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
   ADMIN_PASS: process.env.ADMIN_PASS as string,
   ENV_DEV: false, //TODO: change this into true in production.
+  CORS_ORIGIN: process.env.CORS_ORIGIN as string,
 };
-export const { MONGO_URI, ENV_DEV, JWT_ACCESS_SECRET, PORT } = _config;
+export const { MONGO_URI, ENV_DEV, JWT_ACCESS_SECRET, PORT, CORS_ORIGIN } =
+  _config;
