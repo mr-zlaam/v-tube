324a36a947394d42687486e0dd9f37097cecc873
Zlaam <mrzalaam@gmail.com>
2024-05-31 17:48:13 +0500

    create 1 file, copy 1 file and update 1 file

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+const _config = {
+  PORT: process.env.PORT as string,
+  MONGO_URI: process.env.MONGO_URI as string,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
+  ADMIN_PASS: process.env.ADMIN_PASS as string,
+  ENV_DEV: false, //TODO: change this into true in production.
+};
+export const { MONGO_URI, ENV_DEV, JWT_ACCESS_SECRET, PORT } = _config;
---
+++ src/server/index.ts
@@ -0,0 +1,1 @@
+import { PORT } from "../config";
