761e4facd35c82c232dd2188f43c2ad93f249187
Zlaam <mrzalaam@gmail.com>
2024-06-02 15:29:07 +0500

    update src/CONSTANTS/index.ts

--- src/CONSTANTS/index.ts
+++ src/CONSTANTS/index.ts
@@ -1,7 +1,19 @@
+import { ISDEVELOPMENT_ENVIRONMENT } from "../config";
+
 const DB_NAME = "V-tube";
 const DATA_LIMIT = "50mb";
 const OPT_JWT_REFRESH_TOKEN =
   "JLKHGJLHFUTYOIJJKJVGYjdlkfjkdjkdfjdjfkdj-ee1dfjkdff232328787";
 const OPT_JWT_ACCESS_TOKEN =
   "jfldjfkdjfJDLKSJKLJFLKJD2343423ejlkddfjlkdjflklsdjjflklsdjklfjsdklfj";
-export { DB_NAME, DATA_LIMIT, OPT_JWT_ACCESS_TOKEN, OPT_JWT_REFRESH_TOKEN };
+const COOKIES_OPTION = {
+  httpOnly: true,
+  secure: !ISDEVELOPMENT_ENVIRONMENT && true,
+};
+export {
+  DB_NAME,
+  DATA_LIMIT,
+  OPT_JWT_ACCESS_TOKEN,
+  OPT_JWT_REFRESH_TOKEN,
+  COOKIES_OPTION,
+};
